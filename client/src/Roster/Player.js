2ba365eb93883d8c5b5bd5beeef1cba978dba7f6
Jake Afuvai <jtafuvai@gmail.com>
2019-06-30 01:42:03 -0600

    added edit mode to player card and functionality, there is a bug that is triggering a forced true to showModal after unassigning helmet/shpad... need to find it. also need to add Inventory pages

--- client/src/Roster/Player.js
+++ client/src/Roster/Player.js
@@ -14,12 +14,43 @@ const Player = props => {
         getShoulderpads()
     }, [getShoulderpads])
 
-    const [lastname, setLastname] = useState(props.player.lastname)
-    const [firstname, setFirstname] = useState(props.player.firstname)
-    const [grade, setGrade] = useState(props.player.grade)
+    const [lastname, setLastname] = useState("")
+    const [firstname, setFirstname] = useState("")
+    const [grade, setGrade] = useState("")
+    const [editMode, setEditMode] = useState(false)
     const [selectedHelmet, setSelectedHelmet] = useState("")
     const [selectedShoulderpad, setSelectedShoulderpad] = useState("")
+    const [ol, setol] = useState(false)
+    const [qb, setqb] = useState(false)
+    const [rb, setrb] = useState(false)
+    const [te, sette] = useState(false)
+    const [wr, setwr] = useState(false)
+    const [dl, setdl] = useState(false)
+    const [lb, setlb] = useState(false)
+    const [db, setdb] = useState(false)
+    const [ls, setls] = useState(false)
+    const [k, setk] = useState(false)
+    const [p, setp] = useState(false)
 
+    const olStyle = ol ? {background: "limegreen", transition: "0.3s"} : null
+    const qbStyle = qb ? {background: "limegreen", transition: "0.3s"} : null
+    const rbStyle = rb ? {background: "limegreen", transition: "0.3s"} : null
+    const teStyle = te ? {background: "limegreen", transition: "0.3s"} : null
+    const wrStyle = wr ? {background: "limegreen", transition: "0.3s"} : null
+    const dlStyle = dl ? {background: "limegreen", transition: "0.3s"} : null
+    const lbStyle = lb ? {background: "limegreen", transition: "0.3s"} : null
+    const dbStyle = db ? {background: "limegreen", transition: "0.3s"} : null
+    const lsStyle = ls ? {background: "limegreen", transition: "0.3s"} : null
+    const kStyle = k ? {background: "limegreen", transition: "0.3s"} : null
+    const pStyle = p ? {background: "limegreen", transition: "0.3s"} : null
+
+    const editModeSetter = ({lastname, firstname, grade}) => {
+        setEditMode(!editMode)
+        setLastname(lastname)
+        setFirstname(firstname)
+        setGrade(grade)
+        clearPositions()
+    }
 
     // filtered helmets to display / select only available helmets/spads
     const filteredHelmets = props.stateHelmets
@@ -34,7 +65,8 @@ const Player = props => {
         // .sort((a,b) => a.size.localeCompare(b.size))
 
 
-    // assign / unassign helmet/spad logic    
+    // assign / unassign helmet/spad logic   
+    // maybe add some timeout/suspense to let these functions run full course 
     const handleAssignHelmet = (playerId, playerLN, playerFN, playerSP, helmetString) => {
         const helmetArray = helmetString.split(" ")
         const [helmetId, helmetMake, helmetNumber, helmetSize] = helmetArray
@@ -105,7 +137,6 @@ const Player = props => {
     }
 
 
-    
     const helmets = props.player.helmet && props.player.helmet === "No Helmet Assigned" ?
         <>
         <select name="helmet" onChange={e => setSelectedHelmet(e.target.value)}>
@@ -120,7 +151,6 @@ const Player = props => {
         <>
         <p>{`${props.player.helmet}`}</p><button onClick={() => handleUnassignHelmet(props.player._id, props.player.shoulderpads, props.player.helmet)}>Unassign Helmet</button>
         </>
-
 
 
     const shoulderpads = props.player.shoulderpads && props.player.shoulderpads === "No Shoulderpads Assigned" ?
@@ -145,23 +175,163 @@ const Player = props => {
         : 
         `No Assigned Positions for ${props.player.firstname}`
 
-    
+    const closePlayerCardAndEdit = playerId => {
+        props.closePlayerCardInfo(playerId)
+        setEditMode(false)
+    }
+
+    const clearState = () => {
+        setFirstname("")
+        setLastname("")
+        setGrade("")
+        setol(false)
+        setqb(false)
+        setrb(false)
+        sette(false)
+        setwr(false)
+        setdl(false)
+        setlb(false)
+        setdb(false)
+        setls(false)
+        setk(false)
+        setp(false)
+    }
+    const clearPositions = () => {
+        setol(false)
+        setqb(false)
+        setrb(false)
+        sette(false)
+        setwr(false)
+        setdl(false)
+        setlb(false)
+        setdb(false)
+        setls(false)
+        setk(false)
+        setp(false)
+    }
+
+    const handlePlayerEdits = (playerId) => {
+        let playerPositions = [] 
+        ol && playerPositions.push("OL") 
+        qb && playerPositions.push("QB")
+        rb && playerPositions.push("RB")
+        te && playerPositions.push("TE")
+        wr && playerPositions.push("WR")
+        dl && playerPositions.push("DL")
+        lb && playerPositions.push("LB")
+        db && playerPositions.push("DB")
+        ls && playerPositions.push("LS")
+        k && playerPositions.push("K")
+        p && playerPositions.push("P")
+
+        let updatedPlayer = {
+            lastname,
+            firstname,
+            grade,
+            position: playerPositions
+        }
+        props.editPlayer(playerId, updatedPlayer)
+
+        clearState()
+        updatedPlayer = {}
+        playerPositions = []
+        
+        closePlayerCardAndEdit(playerId)
+        props.getPlayers()
+    }
 
     const modal = props.player.showModal && 
 		<div className="outer-modal">
 			<div className="modal-show">
-				<button onClick={() => props.closePlayerCardInfo(props.player._id)}>X</button>
-                <button>Edit Player Info</button>
+				<button onClick={() => closePlayerCardAndEdit(props.player._id)}>X</button>
+                <button onClick={() => editModeSetter(props.player)}>{!editMode ? "Edit Player Info" : "Cancel Changes"}</button>
+                {!editMode ?
+                <>
 				<h1>{props.player.lastname.toUpperCase()} {props.player.firstname.toLowerCase()}</h1>
 				<h2>Grade: {props.player.grade}</h2>
                 <div>{props.player.position.length > 1 ? "Positions" : "Position"}: {showPositions}</div>
-                <div className="equipment-con">
-                    <h2>Equipment</h2>
-                    <h3>Helmet</h3>
-                    <h4>{props.player.helmet}</h4>
+                </>
+                :
+                <>
+                <h1>{props.player.lastname.toUpperCase()} {props.player.firstname.toLowerCase()}</h1>
+                <input type="text" name="lastname" value={lastname} onChange={e=>setLastname(e.target.value)} placeholder={props.player.lastname}/>
+                <input type="text" name="firstname" value={firstname} onChange={e=>setFirstname(e.target.value)} placeholder={props.player.firstname}/>
+				<h2>Grade: {props.player.grade}</h2>
+                <label><input type="radio" name="grade" value="Senior" checked={grade === "Senior"}onChange={e => setGrade(e.target.value)}/> Senior</label>
+                <label><input type="radio" name="grade" value="Junior" checked={grade === "Junior"}onChange={e => setGrade(e.target.value)}/> Junior</label>
+                <label><input type="radio" name="grade" value="Sophomore" checked={grade === "Sophomore"}onChange={e => setGrade(e.target.value)}/> Sophomore</label>
+                <label><input type="radio" name="grade" value="Freshman" checked={grade === "Freshman"}onChange={e => setGrade(e.target.value)}/> Freshman</label>
+                <div>{props.player.position.length > 1 ? "Positions" : "Position"}: {showPositions}</div>
+                <div className="position-container">
+                    <h4 id="offense">OFFENSE</h4>
+                    <div
+                        id="ol"
+                        style={olStyle}
+                        onClick={() => setol(!ol)}
+                    >OL</div>
+                    <div
+                        id="qb"
+                        style={qbStyle}
+                        onClick={() => setqb(!qb)}
+                    >QB</div>
+                    <div
+                        id="rb"
+                        style={rbStyle}
+                        onClick={() => setrb(!rb)}
+                    >RB</div>
+                    <div
+                        id="te"
+                        style={teStyle}
+                        onClick={() => sette(!te)}
+                    >TE</div>
+                    <div
+                        id="wr"
+                        style={wrStyle}
+                        onClick={() => setwr(!wr)}
+                    >WR</div>
+                    <h4 id="defense">DEFENSE</h4>
+                    <div
+                        id="dl"
+                        style={dlStyle}
+                        onClick={() => setdl(!dl)}
+                    >DL</div>
+                    <div
+                        id="lb"
+                        style={lbStyle}
+                        onClick={() => setlb(!lb)}
+                    >LB</div>
+                    <div
+                        id="db"
+                        style={dbStyle}
+                        onClick={() => setdb(!db)}
+                    >DB</div>
+                    <h4 id="special-teams">SPECIAL TEAMS</h4>
+                    <div
+                        id="ls"
+                        style={lsStyle}
+                        onClick={() => setls(!ls)}
+                    >LS</div>
+                    <div
+                        id="k"
+                        style={kStyle}
+                        onClick={() => setk(!k)}
+                    >K</div>
+                    <div
+                        id="p"
+                        style={pStyle}
+                        onClick={() => setp(!p)}
+                    >P</div>
+                </div>
 
-                    <h3>Shoulder Pads</h3>
-                    <h4>{props.player.shoulderpads}</h4>
+                <button onClick={() => handlePlayerEdits(props.player._id)}>Submit Changes</button>
+                </>
+                }
+                <div className="player-equipment-container">
+                    <h2 className="equip-title">Equipment</h2>
+                    <Helmet className="helmet-icon" height="9vh" />
+                    <h3 className="player-helmet">{props.player.helmet}</h3>
+                    <ShPad className="sp-icon" height="9vh" />
+                    <h3 className="player-sp">{props.player.shoulderpads}</h3>
                 </div>
 			</div>		
 		</div>
@@ -252,7 +422,7 @@ const Player = props => {
             player => player.grade === "Freshman"
         )
         .sort(
-            (a,b) => a.lastname.localCompare(b.lastname)
+            (a,b) => a.lastname.localeCompare(b.lastname)
         )
         .map(
             player => 
